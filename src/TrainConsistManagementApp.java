fb68635303ef0237e2c6bf65420848df09e5cd41
anusharoy24 <anusharc24@gmail.com>
2026-04-07 10:39:34 +0530

    UC7:Sort Bogies by Capacity

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,25 +1,31 @@
-import java.util.LinkedHashSet;
+import java.util.*;
 
-public class PalindromeCheckerApp {
+public class TrainConsistManagementApp {
     public static void main(String[] args) {
 
-        // LinkedHashSet for maintaining order + uniqueness
-        LinkedHashSet<String> trainSet = new LinkedHashSet<>();
+        // Step 1: Create map
+        LinkedHashMap<String, Integer> bogieMap = new LinkedHashMap<>();
+        bogieMap.put("Sleeper", 72);
+        bogieMap.put("AC Chair", 56);
+        bogieMap.put("First Class", 24);
+        bogieMap.put("General", 90);
 
-        // Adding elements (including duplicates)
-        trainSet.add("Engine");
-        trainSet.add("Sleeper");
-        trainSet.add("Cargo");
-        trainSet.add("Guard");
-        trainSet.add("Sleeper"); // duplicate
-        trainSet.add("Engine");  // duplicate
+        // Before Sorting
+        System.out.println("Before Sorting");
+        for (Map.Entry<String, Integer> entry : bogieMap.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
 
-        System.out.println("Final Train Formation:");
-        System.out.println(trainSet);
+        // Step 2: Convert to list
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(bogieMap.entrySet());
 
-        System.out.println("\nNote:");
-        System.out.println("LinkedHashSet, Preserves Insertion order and removes duplicates automatically.");
+        // Step 3: Sort by value
+        Collections.sort(list, (a, b) -> a.getValue() - b.getValue());
 
-        System.out.println("\nUCS formation setup completed.....");
+        // After Sorting
+        System.out.println("\nAfter Sorting By Capacity");
+        for (Map.Entry<String, Integer> entry : list) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
     }
 }
