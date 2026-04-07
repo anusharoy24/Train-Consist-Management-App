2661fe26cd720645f92cebcf70f97f047bfd3392
anusharoy24 <anusharc24@gmail.com>
2026-04-07 10:16:48 +0530

    UC5:Mintain Ordered Bogie IDs (TreeSet & SortedSet)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,29 +1,25 @@
-import java.util.LinkedList;
+import java.util.LinkedHashSet;
 
-public class TrainConsistManagementApp {
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
-        // Create LinkedList for train consist
-        LinkedList<String> train = new LinkedList<>();
+        // LinkedHashSet for maintaining order + uniqueness
+        LinkedHashSet<String> trainSet = new LinkedHashSet<>();
 
-        // Initial elements
-        train.add("Engine");
-        train.add("Sleeper");
-        train.add("AC");
-        train.add("Cargo");
-        train.add("Guard");
+        // Adding elements (including duplicates)
+        trainSet.add("Engine");
+        trainSet.add("Sleeper");
+        trainSet.add("Cargo");
+        trainSet.add("Guard");
+        trainSet.add("Sleeper"); // duplicate
+        trainSet.add("Engine");  // duplicate
 
-        System.out.println("Initial Train Consist;");
-        System.out.println(train);
+        System.out.println("Final Train Formation:");
+        System.out.println(trainSet);
 
-        // Remove first and last bogie
-        train.removeFirst();   // removes Engine
-        train.removeLast();    // removes Guard
+        System.out.println("\nNote:");
+        System.out.println("LinkedHashSet, Preserves Insertion order and removes duplicates automatically.");
 
-        // Adding Pantry Car (as shown in expected output)
-        train.add(1, "Pantry Car");
-
-        System.out.println("\nAfter Removing First and last Bogie:");
-        System.out.println(train);
+        System.out.println("\nUCS formation setup completed.....");
     }
 }
