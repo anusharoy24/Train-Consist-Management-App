46257bd2da667c8012e21f369ddfba3fa5cb9889
anusharoy24 <anusharc24@gmail.com>
2026-04-07 09:01:59 +0530

    Initialize Train and Display Consist Summary

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,8 @@
 public class TrainConsistManagementApp {
+    public static void main(String[] args) {
+        System.out.println("Train Initialised successfully");
+        System.out.println("Initial Budget count : 0");
+        System.out.println("Current Train consist : []");
+        System.out.println("system ready for operation");
+    }
 }
